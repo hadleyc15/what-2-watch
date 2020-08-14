5a08162548ac8cc45befc58bca9e6ca0555b82c4
Chris Hadley <hadleyc15@yahoo.com>
2020-08-13 18:43:12 -0600

    got the countdown to work forever

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,32 +7,36 @@ const {
 const withAuth = require('../../utils/auth');
 const countdown = require('countdown');
 
-// function updateShowTime (date = new Date(2020, 6, 31, 19), day = new Date()) {
-//   console.log(date)
-//   // if we've passed the default date
-//   if (date < day) {
-//     let newDate = date.setDate(date.getDate() + 7)
-//    // change the format of newDate back into the format originally passed into this function
-   
-//     console.log(newDate)
-//     if (newDate < day) {
-//       updateShowTime(newDate)
-//     } else {
-//       return newDate
-//     }
-//   } else {
-//     return date;
-//   } 
+let dateOfNextShowing = "";
+function updateShowTime(date = new Date(2020, 6, 31, 19), day = new Date()) {
+  dateOfNextShowing = date;
+  console.log(dateOfNextShowing)
+  // if we've passed the default date
+  if (date < day) {
+    let mSec = date.setDate(date.getDate() + 7)
+    let newDate = new Date(mSec)
+    // change the format of newDate back into the format originally passed into this function
 
-// }
+    console.log(newDate)
+    if (newDate < day) {
+      updateShowTime(newDate)
+    } else {
+      return newDate
+    }
+  } else {
+    return date;
+  }
+}
+updateShowTime();
 
+let timeLeftToVote = countdown(null, dateOfNextShowing, ~(countdown.SECONDS | countdown.MILLISECONDS)).toString();
 // GET http://localhost:3001/api/users/countdown
 router.get('/countdown', (req, res) => {
   let date = new Date(2020, 7, 14, 19);
   let now = new Date();
   if (date < now) {
     date.setDate(date.getDate() + 7);
-  } 
+  }
   let timeLeftToVote = countdown(null, date, ~(countdown.SECONDS | countdown.MILLISECONDS)).toString();
 
   function myFunction() {
@@ -46,10 +50,10 @@ router.get('/countdown', (req, res) => {
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
   User.findAll({
-      attributes: {
-        exclude: ['password']
-      }
-    })
+    attributes: {
+      exclude: ['password']
+    }
+  })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -60,24 +64,24 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
-      attributes: {
-        exclude: ['password']
-      },
-      where: {
-        id: req.params.id
-      },
-      include: [{
-          model: Post,
-          attributes: ['id', 'title', 'post_url', 'created_at']
-        },
-        {
-          model: Post,
-          attributes: ['title'],
-          through: Vote,
-          as: 'voted_posts'
-        }
-      ]
-    })
+    attributes: {
+      exclude: ['password']
+    },
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Post,
+      attributes: ['id', 'title', 'post_url', 'created_at']
+    },
+    {
+      model: Post,
+      attributes: ['title'],
+      through: Vote,
+      as: 'voted_posts'
+    }
+    ]
+  })
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({
@@ -98,10 +102,10 @@ router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   console.log(req)
   User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    })
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
     .then(dbUserData => {
       req.session.save(() => {
         req.session.user_id = dbUserData.id;
@@ -160,11 +164,11 @@ router.put('/:id', withAuth, (req, res) => {
 
   // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
   User.update(req.body, {
-      individualHooks: true,
-      where: {
-        id: req.params.id
-      }
-    })
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
     .then(dbUserData => {
       if (!dbUserData[0]) {
         res.status(404).json({
@@ -183,10 +187,10 @@ router.put('/:id', withAuth, (req, res) => {
 // DELETE /api/users/1
 router.delete('/:id', withAuth, (req, res) => {
   User.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
+    where: {
+      id: req.params.id
+    }
+  })
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({
